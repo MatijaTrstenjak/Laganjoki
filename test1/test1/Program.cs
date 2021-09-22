04b04f82141f4a4721ae88708a90e9ac47549c6a
Matija Trstenjak <84410644+MatijaTrstenjak@users.noreply.github.com>
2021-09-22 13:01:00 +0200

    Add files via upload

---
+++ test1/test1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+//funkcija koja prima argument i promijeni ga
+namespace test1 {
+    class Program {
+
+        static string funkcija(string brojcek) {
+            char[] charArr = brojcek.ToCharArray();
+            int komplementPrvi = 0;
+
+            for(int i = 0; i<brojcek.Length; i++) 
+            {
+                if (charArr[i] == '0')
+                    charArr[i] = '1';
+                else
+                    charArr[i] = '0';
+            }
+
+            string komplement = new string(charArr);
+
+            int binarno = Int32.Parse(charArr);
+            int dekadsko = Convert.ToInt32(komplement, 2);
+
+            dekadsko++;
+
+            string dvojniKomplement = Convert.ToString(dekadsko, 2);
+            return dvojniKomplement;
+        }
+
+        static void Main(string[] args) {
+            string broj;
+            broj = Console.ReadLine();
+            Console.WriteLine(funkcija(broj));
+                
+        }
+    }
+}
